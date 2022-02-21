c22c26e58a4aca9ce1dc3a3705612a540f3e301e
frankfang <frankfang1990@gmail.com>
2022-02-21 22:16:14 +0800

    实现文本上传

--- main.go
+++ main.go
@@ -3,24 +3,30 @@ package main
 import (
 	"embed"
 	"io/fs"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
+	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 //go:embed frontend/dist/*
 var FS embed.FS
 
 func main() {
+
 	go func() {
 		gin.SetMode(gin.DebugMode)
 		router := gin.Default()
 		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		router.POST("/api/v1/texts", TextsController)
 		router.StaticFS("/static", http.FS(staticFiles))
 		router.NoRoute(func(c *gin.Context) {
 			path := c.Request.URL.Path
@@ -54,4 +60,35 @@ func main() {
 	case <-chSignal:
 		cmd.Process.Kill()
 	}
+}
+
+func TextsController(c *gin.Context) {
+	var json struct {
+		Raw string `json:"raw"`
+	}
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	} else {
+		exe, err := os.Executable() // 获取当前执行文件的路径
+		if err != nil {
+			log.Fatal(err)
+		}
+		dir := filepath.Dir(exe) // 获取当前执行文件的目录
+		if err != nil {
+			log.Fatal(err)
+		}
+		filename := uuid.New().String()          // 生成一个文件名
+		uploads := filepath.Join(dir, "uploads") // 拼接 uploads 的绝对路径
+		err = os.MkdirAll(uploads, os.ModePerm)  // 创建 uploads 目录
+		if err != nil {
+			log.Fatal(err)
+		}
+		fullpath := path.Join("uploads", filename+".txt")                            // 拼接文件的绝对路径（不含 exe 所在目录）
+		err = ioutil.WriteFile(filepath.Join(dir, fullpath), []byte(json.Raw), 0644) // 将 json.Raw 写入文件
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath}) // 返回文件的绝对路径（不含 exe 所在目录）
+	}
+
 }
