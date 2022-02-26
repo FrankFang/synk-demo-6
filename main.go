e604ebb0c28d2cbc8063fb29a8ae45e9904daee4
frankfang <frankfang1990@gmail.com>
2022-02-26 22:12:54 +0800

    ctrl C 和关闭按钮都可以完美退出

--- main.go
+++ main.go
@@ -10,21 +10,34 @@ import (
 )
 
 func main() {
-	go server.Run()
-	cmd := startBrowser()
+	chChromeDie := make(chan struct{})
+	chBackendDie := make(chan struct{})
 	chSignal := listenToInterrupt()
-	select {
-	case <-chSignal:
-		cmd.Process.Kill()
+	go server.Run()
+	go startBrowser(chChromeDie, chBackendDie)
+	for {
+		select {
+		case <-chSignal:
+			chBackendDie <- struct{}{}
+		case <-chChromeDie:
+			os.Exit(0)
+		}
 	}
 }
 
-func startBrowser() *exec.Cmd {
+func startBrowser(chChromeDie chan struct{}, chBackendDie chan struct{}) {
 	// 先写死路径，后面再照着 lorca 改
 	chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:"+config.GetPort()+"/static/index.html")
 	cmd.Start()
-	return cmd
+	go func() {
+		<-chBackendDie
+		cmd.Process.Kill()
+	}()
+	go func() {
+		cmd.Wait()
+		chChromeDie <- struct{}{}
+	}()
 }
 
 func listenToInterrupt() chan os.Signal {
